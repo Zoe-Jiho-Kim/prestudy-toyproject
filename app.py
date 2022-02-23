14199155f5dad1930d2ecb446b7bbbc0c0b59e1c
yurim <dbfla5036@naver.com>
2022-02-23 20:42:08 +0900

    댓글남기기 1차완료 .. 순번 받아오는 건 어렵네요ㅠㅠ.. 일단 댓글만 남겨집니다. ^^;;

--- app.py
+++ app.py
@@ -1,1 +1,32 @@
-print("hello world!")
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.7fswg.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/toon", methods=["POST"])
+def toon_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+
+
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive
+    }
+
+    db.toon.insert_one(doc)
+    return jsonify({'msg': '댓글 남기기!'})
+
+@app.route("/toon", methods=["GET"])
+def toon_get():
+    comment_list = list(db.toon.find({}, {'_id': False}))
+    return jsonify({'comment':comment_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,81 @@
+'use strict';
+// genre btn toggle function
+$(function () {
+  $('.genre__btn').click(function () {
+    $('.genre__content').slideToggle('2000');
+  });
+});
+
+// 모달 창이 열리면 input에 autofocus
+// const myModal = document.getElementById('myModal');
+// const myInput = document.getElementById('myInput');
+
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus();
+// });
+
+// 모달창에 웹툰 제목이 뜬다
+const exampleModal = document.getElementById('exampleModal');
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  const button = event.relatedTarget;
+  // Extract info from data-bs-* attributes
+  const recipient = button.getAttribute('data-bs-whatever');
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  const modalTitle = exampleModal.querySelector('.modal-title');
+  // const modalBodyInput = exampleModal.querySelector('.modal-body input');
+
+  modalTitle.textContent = recipient;
+  //modalBodyInput.value = recipient;
+});
+
+
+
+//댓글ajax영역
+$(document).ready(function () {
+  show_comment();
+  $('#commentBtn').on('click',save_comment)
+
+});
+
+
+function save_comment() {
+  let name = $('#recipient-name').val()
+  let comment = $('#message-text').val()
+
+  $.ajax({
+    type: 'POST',
+    url: '/toon',
+    data: {'name_give': name, 'comment_give': comment},
+    success: function (response) {
+      alert(response['msg'])
+      window.location.reload()
+    }
+  })
+}
+
+function show_comment() {
+  $.ajax({
+    type: "GET",
+    url: "/toon",
+    data: {},
+    success: function (response) {
+      let rows = response["comment"]
+      for (let i = 0; i < rows.length; i++) {
+        let name = rows[i]['name']
+        let comment = rows[i]['comment']
+
+        let temp_html = `<div class="row">
+                          <div class="col user-name">${name}</div>
+                          <div class="col-9">${comment}</div>
+                        </div>`
+
+        $('#comment_box').prepend(temp_html)
+      }
+
+    }
+  });
+}
