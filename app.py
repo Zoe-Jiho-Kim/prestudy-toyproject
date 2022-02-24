76eb9dcb51f4f9dbd51c85a6564ac328efaf07ee
Juhwan <dev.juhwanlee@gmail.com>
2022-02-24 19:37:30 +0900

    Delete useless code app.py

--- app.py
+++ app.py
@@ -120,29 +120,14 @@ def toon_get():
     return jsonify({'comment': comment_list})
 
 
+
+
+
+
+    
 #########################################################
 # 실행 코드 (맨 아래에 위치해야합니다)
 #########################################################
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-
-
-########################################################
-#
-# 춘님 (02.24.07:53 현재 코드가 동작하지 않습니다)
-#
-#########################################################
-
-clientc = MongoClient(
-    'mongodb+srv://test:sparta@cluster0.rtpl1.mongodb.net/Cluster0?retryWrites=true&w=majority')
-dbc = clientc.dbsparta
-
-
-@app.route("/webtoons", methods=["GET"])
-def webtoon_get():
-    webtoon_list = list(db.webtoons.find({}, {'_id': False}))
-    return jsonify({'webtoons':webtoon_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
