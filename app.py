9bf3f8b8c27ad3c214a0d760721322f4cddfac66
kiwon kwon <funnykyeon@naver.com>
2022-02-25 20:18:17 +0900

    정보변경페이지 └상단 나의 닉네임로딩,이메일박제,비밀번호 유효성검사,비밀번호 변경 index페이지 └상단 나의 닉네임로딩,회원정보수정란 생성 v.4

--- app.py
+++ app.py
@@ -75,9 +75,9 @@ def information():
 
             return render_template('information.html', email=user_info["id"], nickname=user_info["nick"])
         except jwt.ExpiredSignatureError:
-            return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+            return redirect(url_for("main", msg="로그인 시간이 만료되었습니다."))
         except jwt.exceptions.DecodeError:
-            return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+            return redirect(url_for("main", msg="로그인 정보가 존재하지 않습니다."))
 
 @app.route('/main')
 def main():
