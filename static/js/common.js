e3be8594d3b5569e8b25c7d9a0014df72cff3d71
yurim <dbfla5036@naver.com>
2022-02-27 16:23:39 +0900

    회원정보 보여주고 숨기기 함수 추가

--- static/js/common.js
+++ static/js/common.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    naviEffect();
-
+    naviEffect(); //00님 환영합니다. =>마우스 오버시 서브메뉴(로그아웃 버튼, 회원가입수정 버튼 등장)
+    memberShowHide();
   });
 
  function naviEffect() {
@@ -21,4 +21,33 @@ document.addEventListener('DOMContentLoaded',()=>{
      function mainMenuOut(){
          gsap.set(memberBox,{display:'none'});
      }
- }
+ }; //naviEffect
+
+ function memberShowHide(){
+     //변수 선언
+     let nickinfo = document.querySelector('#nickinfo');
+     let nick = nickinfo.innerHTML;
+     let isExist = isNaN(nick); //nick의 값이 없다면 (로그인X) => false값 받고, nick 값이 있다면(로그인 O) =>true값 받는다.
+     const logBtn = document.querySelector('#logBtn');
+     const memberinfo = document.querySelector('#memberinfo');
+     const logout=document.querySelector('#logout>a')
+    // console.log(isExist);
+
+    if(isExist == false){
+        logBtn.style.display="block";
+        memberinfo.style.display="none";
+    }else{
+         logBtn.style.display="none";
+         memberinfo.style.display="block";
+    }
+    initEvent(); //이벤트 함수
+
+    function initEvent(){
+        logout.addEventListener('click',memberHide)
+     }; //initEvent
+
+     function memberHide(){
+        logBtn.style.display="block";
+        memberinfo.style.display="none";
+     }; //memberHide
+}; //memberShowHide
