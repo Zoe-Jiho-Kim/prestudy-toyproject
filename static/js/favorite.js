0be8ffaf8764fdaa6426e6aec8fa87e6c7443cdf
Juhwan <dev.juhwanlee@gmail.com>
2022-03-02 19:30:32 +0900

    Add function: show favorite list

--- static/js/favorite.js
+++ static/js/favorite.js
@@ -1,24 +1,6 @@
 'use strict';
 
 //작업중
-$('#favorites').on('click', save_favorites);
-// comment 저장 함수
-function save_favorites() {
-  let title = titleBucket;
-  let name = document.querySelector('#useremail').innerHTML;
-
-  $.ajax({
-    type: 'POST',
-    url: '/favorites',
-    data: {
-      name_give: name,
-      title_give: title,
-    },
-    success: function (response) {
-      alert(response['msg']);
-    },
-  });
-}
 
 //////////////////////////////////////////////////
 $(document).ready(function () {
@@ -26,6 +8,8 @@ $(document).ready(function () {
 });
 
 function list() {
+  $('#thumbnail-box').empty();
+  moreBtn.className = 'blind';
   $.ajax({
     type: 'GET',
     url: '/favoritelist',
@@ -41,8 +25,46 @@ function list() {
           data: { title_give: title },
           success: function (response) {
             let rows = response['webtoons'][0];
-            console.log(rows);
 
+            let title = rows['title'];
+            let body = rows['body'].replace(/\"/gi, "'"); // Change double quotes to single quotes
+            let img = rows['img'];
+            let writer = rows['writer'];
+            let url = rows['url'];
+            let star = rows['star'];
+            let genre = rows['genre'];
+            let temp_html = `<button
+                                  type="button"
+                                  class="thumbnail"
+                                  data-bs-toggle="modal"
+                                  data-bs-target="#exampleModal"
+                                  data-bs-whatever="${title}"
+                                  data-writer="${writer}"
+                                  data-body="${body}"
+                                  data-url="${url}"
+                                  data-star="⭐${star}"
+                                  data-genre="${genre}"
+                                  data-img="${img}"
+                                >
+                                  <div class="col">
+                                    <div class="card shadow-sm">
+                                      <img
+                                        src="${img}"
+                                        width="100%"
+                                        height="100%"
+                                        title="${title}"
+                                        alt="${title}"
+                                      />
+                                      <div class="card-body">
+                                        <p class="thunmbnail__title card-text">${title}</p>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </button>`;
+            $('#thumbnail-box').append(temp_html);
+
+            readTitle();
+            viewComments();
           },
         });
       }
--- static/js/main.js
+++ static/js/main.js
@@ -51,64 +51,6 @@ exampleModal.addEventListener('show.bs.modal', function (event) {
 });
 
 /*************************
- * 춘님 db에서 크롤링해온 데이터 받아오기
- **************************/
-$(document).ready(function () {
-  listing();
-});
-
-function listing() {
-  $.ajax({
-    type: 'GET',
-    url: '/webtoons',
-    data: {},
-    success: function (response) {
-      let rows = response['webtoons'];
-      for (let i = 0; i < 30; i++) {
-        let title = rows[i]['title'];
-        let body = rows[i]['body'].replace(/\"/gi, "'"); // Change double quotes to single quotes
-        let img = rows[i]['img'];
-        let writer = rows[i]['writer'];
-        let url = rows[i]['url'];
-        let star = rows[i]['star'];
-        let genre = rows[i]['genre'];
-        let temp_html = `<button
-                            type="button"
-                            class="thumbnail"
-                            data-bs-toggle="modal"
-                            data-bs-target="#exampleModal"
-                            data-bs-whatever="${title}"
-                            data-writer="${writer}"
-                            data-body="${body}"
-                            data-url="${url}"
-                            data-star="⭐${star}"
-                            data-genre="${genre}"
-                            data-img="${img}"
-                          >
-                            <div class="col">
-                              <div class="card shadow-sm">
-                                <img
-                                  src="${img}"
-                                  width="100%"
-                                  height="100%"
-                                  title="${title}"
-                                  alt="${title}"
-                                />
-                                <div class="card-body">
-                                  <p class="thunmbnail__title card-text">${title}</p>
-                                </div>
-                              </div>
-                            </div>
-                          </button>`;
-        $('#thumbnail-box').append(temp_html);
-      }
-      readTitle();
-      viewComments();
-    },
-  });
-}
-
-/*************************
  * moreBtn function
  **************************/
 
@@ -418,3 +360,26 @@ $(document).ready(function () {
     window.location.href = '/';
   });
 });
+
+/*************************
+ * favorite 저장함수
+ **************************/
+
+$('#favorites').on('click', save_favorites);
+// comment 저장 함수
+function save_favorites() {
+  let title = titleBucket;
+  let name = document.querySelector('#useremail').innerHTML;
+
+  $.ajax({
+    type: 'POST',
+    url: '/favorites',
+    data: {
+      name_give: name,
+      title_give: title,
+    },
+    success: function (response) {
+      alert(response['msg']);
+    },
+  });
+}
