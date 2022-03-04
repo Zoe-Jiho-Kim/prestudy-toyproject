495837084d55092d1fb66d92d3692f2d4c98cc3f
kiwon kwon <funnykyeon@naver.com>
2022-03-04 10:48:45 +0900

    readme 작성 및 token decode 수정

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import json
 import pandas as pd
 import mechanicalsoup
@@ -102,7 +103,7 @@ def api_information():
     if result is not None:
         payload = {
             'id': id_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=24 * 60 * 60)
+            'exp': datetime.datetime.utcnow() + timedelta(seconds=24 * 60 * 60)
         }
         token = encode(payload, SECRET_KEY, algorithm='HS256')
 
@@ -193,23 +194,22 @@ def login():
 
 @app.route('/api/login', methods=['post'])
 def api_login():
-
     id_receive = request.form['id_give']
     pw_receive = request.form['pw_give']
 
     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
 
     result = toonUser.find_one({'id': id_receive, 'pw': pw_hash})
-    print(result)
+
     if result is not None:
         payload = {
             'id': id_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60*60*24)
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=24 * 60 * 60)
         }
-        token = encode(payload, SECRET_KEY, algorithm='HS256')
-        print(payload)
-        return jsonify({'result': 'success', 'token': token})
+        token = encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+                                                        ##### .decode('utf-8') 은 로컬에선 빼주시고 ubuntu에만 넣어주세요
 
+        return jsonify({'result': 'success', 'token': token})
     else:
 
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
@@ -360,3 +360,4 @@ def search():
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
